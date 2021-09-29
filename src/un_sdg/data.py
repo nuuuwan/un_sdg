7112200ee8906778a3727d1444c7467596e2f092
nuuuwan <nuuuwan@gmail.com>
2021-09-29 12:19:00 +0530

    [main] build_spreadsheet complete (2021-09-29-1219)

--- src/un_sdg/data.py
+++ src/un_sdg/data.py
@@ -1,5 +1,6 @@
 import re
 
+import xlsxwriter
 from utils import filex, jsonx
 
 from un_sdg._utils import log
@@ -8,6 +9,29 @@ REGEX_L1 = r'^Goal (?P<goal_num>\d{1,2})\.\s(?P<goal_description>.+)'
 REGEX_L2 = r'^(?P<goal_num>\d{1,2}\.\w{1})\s(?P<goal_description>.+)'
 REGEX_L3 = r'^(?P<goal_num>\d{1,2}\.\w{1}\.\d{1})\s(?P<goal_description>.+)'
 REGEX_INDICATOR = r'C\w{6}'
+
+DATA_INDEX_FILE = 'src/un_sdg/data/data_index.json'
+
+
+L1_GOAL_NUM_TO_SHORT = {
+    '1': 'poverty',
+    '2': 'hunger',
+    '3': 'health',
+    '4': 'education',
+    '5': 'gender',
+    '6': 'water-sanit',
+    '7': 'energy',
+    '8': 'economy',
+    '9': 'infrastructure',
+    '10': 'inequality',
+    '11': 'cities',
+    '12': 'consumption',
+    '13': 'climate',
+    '14': 'oceans',
+    '15': 'forrests',
+    '16': 'peace',
+    '17': 'partnership',
+}
 
 
 def load_raw():
@@ -32,7 +56,7 @@ def load_raw():
                 l1_goals[goal_num] = dict(
                     goal_num=goal_num,
                     goal_description=goal_description,
-                    children={},
+                    child_l2_goals={},
                 )
                 continue
 
@@ -44,7 +68,7 @@ def load_raw():
                 l2_goals[goal_num] = dict(
                     goal_num=goal_num,
                     goal_description=goal_description,
-                    children={},
+                    child_l3_goals={},
                 )
                 continue
 
@@ -67,16 +91,90 @@ def load_raw():
 
     for l2_goal_num, l2_goal in l2_goals.items():
         l1_goal_num = '.'.join(l2_goal_num.split('.')[:-1])
-        l1_goals[l1_goal_num]['children'][l2_goal_num] = l2_goal
+        l1_goals[l1_goal_num]['child_l2_goals'][l2_goal_num] = l2_goal
 
     for l3_goal_num, l3_goal in l3_goals.items():
         l2_goal_num = '.'.join(l3_goal_num.split('.')[:-1])
-        l2_goals[l2_goal_num]['children'][l3_goal_num] = l3_goal
+        l2_goals[l2_goal_num]['child_l3_goals'][l3_goal_num] = l3_goal
 
-    data_index_file = 'src/un_sdg/data/data_index.json'
-    jsonx.write(data_index_file, l1_goals)
-    log.info(f'Wrote data index to {data_index_file}...')
+    jsonx.write(DATA_INDEX_FILE, l1_goals)
+    log.info(f'Wrote data index to {DATA_INDEX_FILE}')
+
+    return l1_goals
+
+
+def load():
+    return jsonx.read(DATA_INDEX_FILE)
+
+
+def build_line(cells):
+    return '\t'.join(
+        list(map(str, cells)),
+    )
+
+
+def build_spreadsheet():
+    data_index = load()
+    spreadsheet_file = 'src/un_sdg/data/un_sdg.xlsx'
+    workbook = xlsxwriter.Workbook(spreadsheet_file)
+    _generic = {
+        'align': 'left',
+        'valign': 'top',
+        'text_wrap': True,
+        'font_name': 'Lato',
+    }
+    _l1 = workbook.add_format(
+        _generic | {'bold': True, 'font_size': 18, 'font_color': '#c0c0c0'}
+    )
+    _l2 = workbook.add_format(
+        _generic | {'bold': True, 'font_size': 12, 'font_color': '#808080'}
+    )
+    _l3 = workbook.add_format(
+        _generic
+        | {
+            'bold': False,
+            'font_size': 12,
+            'font_color': '#000000',
+            'bg_color': '#F0F8FF',
+            'border': True,
+            'border_color': '#c0c0c0',
+        }
+    )
+
+    for l1_goal_num, l1_goal in data_index.items():
+        short_name = L1_GOAL_NUM_TO_SHORT.get(l1_goal_num, '')
+        worksheet_label = f'{l1_goal_num}-{short_name}'
+        worksheet = workbook.add_worksheet(worksheet_label)
+
+        for i_col in range(1, 20, 2):
+            worksheet.set_column(i_col, i_col, 6)
+            worksheet.set_column(i_col + 1, i_col + 1, 48)
+
+        i_row = 0
+        i_col = 1
+        worksheet.write(i_row, i_col, l1_goal_num, _l1)
+        i_col += 1
+        worksheet.write(i_row, i_col, l1_goal['goal_description'], _l1)
+
+        for l2_goal_num, l2_goal in l1_goal['child_l2_goals'].items():
+
+            i_row += 1
+            i_col = 1
+            worksheet.write(i_row, i_col, l2_goal_num, _l2)
+            i_col += 1
+            worksheet.write(i_row, i_col, l2_goal['goal_description'], _l2)
+            i_col += 1
+
+            for l3_goal_num, l3_goal in l2_goal['child_l3_goals'].items():
+                worksheet.write(i_row, i_col, l3_goal_num, _l3)
+                i_col += 1
+                worksheet.write(i_row, i_col, l3_goal['goal_description'], _l3)
+                i_col += 1
+
+    workbook.close()
+    log.info(f'Wrote spreadsheet to {spreadsheet_file}')
 
 
 if __name__ == '__main__':
     load_raw()
+    build_spreadsheet()
